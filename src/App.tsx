68947e7574cd9421e8e86731f76cc73174cc9416
durgarao310 <gantadurgarao304@gmail.com>
2024-03-06 08:07:09 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+        Material UI Vite.js example in TypeScript
+      </Typography>
+    </>
+  );
+}
+
+export default App;
